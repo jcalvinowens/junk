890482865c10d91243442e001fd6bb84161a925f
Calvin Owens <jcalvinowens@gmail.com>
2023-12-18 21:37:32 -0800

    Clean up, split some stuff into new repos

--- chess/main.c
+++ chess/main.c
@@ -1,18 +1,17 @@
-/* chess-engine: A (currently very simplistic) chess AI
+/*
+ * chess-engine: A (currently very simplistic) chess AI
+ * Copyright (C) 2013 Calvin Owens <jcalvinowens@gmail.com>
  *
- * Copyright (C) 2013 Calvin Owens
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This program is free software: you can redistribute it and/or modify it
- * under the terms of VERSION 2 ONLY of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER(S) OR AUTHOR(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THIS SOFTWARE. */
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
--- radio/cwutil.py
+++ radio/cwutil.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
+
+# Written by Calvin Owens <jcalvinowens@gmail.com>
 #
-# Copyright (C) 2017 Calvin Owens <jcalvinowens@gmail.com>
+# To the extent possible under law, I waive all copyright and related or
+# neighboring rights. You should have received a copy of the CC0 license along
+# with this work. If not, see http://creativecommons.org/publicdomain/zero/1.0
 #
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; version 2 only.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-# details.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import argparse
 import itertools
